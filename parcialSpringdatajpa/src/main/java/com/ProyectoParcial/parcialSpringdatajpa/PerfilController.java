82740306fef0c966a8e5f5f4dc8ecccea82368b4
Aaron Lazo <zevallos.lazo.aaron@gmail.com>
2023-05-29 05:59:23 -0500

    se modificó la ruta de mapeo - ahora todas las rutas siguen mismo formato {/api/v1/myEntity}

--- parcialSpringdatajpa/src/main/java/com/ProyectoParcial/parcialSpringdatajpa/PerfilController.java
+++ parcialSpringdatajpa/src/main/java/com/ProyectoParcial/parcialSpringdatajpa/PerfilController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/perfiles")
+@RequestMapping("/api/v1/perfiles")
 public class PerfilController {
 
     @Autowired
--- parcialSpringdatajpa/src/main/java/com/ProyectoParcial/parcialSpringdatajpa/ReservaController.java
+++ parcialSpringdatajpa/src/main/java/com/ProyectoParcial/parcialSpringdatajpa/ReservaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservas")
+@RequestMapping("/api/v1/reservas")
 
 public class ReservaController {
     @Autowired
